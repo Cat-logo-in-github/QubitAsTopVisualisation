6bb0d47c62f4ad88accb297140e02cf869dcb5b7
Cat-logo-in-github <parshatwork@gmail.com>
2024-05-13 19:51:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ rotateX(-30);
     rotateY(frameCount * 3);
     else if(frameCount>380 && frameCount<950){
     background(255,0,0);
-    rotateY(frameCount * (3-(i*0.09)));
-    rotateZ(1/3*i);
+    rotateY(frameCount * (3-(i*0.002)));
+    rotateZ(1/57*i);
     i++;
     }
     else if(frameCount>950 && frameCount<1200){
